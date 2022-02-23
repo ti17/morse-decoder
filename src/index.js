d3152aa8fd94cab83f67b9ca670d41d0a36d03a2
ti17 <95610643+ti17@users.noreply.github.com>
2022-02-23 16:36:08 +0300

    feat: done js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const morsArr = Object.values(MORSE_TABLE);
+    const morsNameArr = Object.getOwnPropertyNames(MORSE_TABLE);
+    let result = '';
+    let result1 = '';
+    
+    expr = expr.match(/.{1,10}/g);
+    
+    expr.forEach((e) => {
+    result = e.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*/g, '');
+        for (let i = 0; i < morsArr.length; i++) {
+            if (result === '') { result1 += ' '; break }
+            if (result === morsNameArr[i]) {
+                result1 += morsArr[i];
+                break
+            }
+        }
+        result = '';
+    });
+    
+    return result1;
+    }
+
 
 module.exports = {
     decode
